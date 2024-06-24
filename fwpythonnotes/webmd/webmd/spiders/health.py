77a40e8344ea47ea6e9296cb924786f67b2d8e46
fadhuweb <f.abdulazee@alustudent.com>
2024-06-24 16:14:15 +0100

    python
    python

---
+++ fwpythonnotes/webmd/webmd/spiders/health.py
@@ -0,0 +1,19 @@
+import scrapy
+
+class WebMDSpider(scrapy.Spider):
+    name = "webmd"
+    start_urls = ['https://www.webmd.com/a-to-z-guides/health-topics']
+
+    def parse(self, response):
+        for link in response.css('a::attr(href)').getall():
+            if '/a-to-z-guides/' in link:
+                yield response.follow(link, self.parse_topic)
+
+    def parse_topic(self, response):
+        yield {
+            'title': response.css('h1::text').get(),
+            'description': response.css('div.article-section p::text').getall(),
+            'symptoms': response.css('div.symptoms-section ul li::text').getall(),
+            'causes': response.css('div.causes-section ul li::text').getall(),
+            'treatments': response.css('div.treatments-section ul li::text').getall()
+        }
